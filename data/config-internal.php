14d5e23e7ef60472823ece24172290447c8fd6ba
skoksharov <semkoksharov.dev@gmail.com>
2026-01-29 00:57:34 +0100

    pre-config, ddev project creation

---
+++ data/config-internal.php
@@ -0,0 +1,34 @@
+<?php
+return [
+  'database' => [
+    'host' => 'localhost',
+    'port' => NULL,
+    'charset' => NULL,
+    'dbname' => '',
+    'user' => '',
+    'password' => ''
+  ],
+  'smtpPassword' => NULL,
+  'logger' => [
+    'path' => 'data/logs/espo.log',
+    'level' => 'WARNING',
+    'rotation' => true,
+    'maxFileNumber' => 30,
+    'printTrace' => false,
+    'databaseHandler' => false,
+    'sql' => false,
+    'sqlFailed' => false
+  ],
+  'restrictedMode' => false,
+  'cleanupAppLog' => true,
+  'cleanupAppLogPeriod' => '30 days',
+  'webSocketMessager' => 'ZeroMQ',
+  'clientSecurityHeadersDisabled' => false,
+  'clientCspDisabled' => false,
+  'clientCspScriptSourceList' => [
+    0 => 'https://maps.googleapis.com'
+  ],
+  'adminUpgradeDisabled' => false,
+  'isInstalled' => false,
+  'microtimeInternal' => 1769644486.069699
+];
